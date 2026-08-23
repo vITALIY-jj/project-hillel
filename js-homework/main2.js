a45bfe0c2846117dedca2ed3ec0a5a9870dfbefd
vITALIY-jj <vitalijsogenko@gmail.com>
2026-08-23 19:04:10 +0300

    start js

---
+++ js-homework/main2.js
@@ -0,0 +1,16 @@
+let myNum = 10;
+let myStr = 'some string';
+let myBool = true;
+let myArr = [1, 2, 3, 4, 5];
+
+let myObj = {
+    first: 'First Name',
+    last: 'Last Name'
+};
+
+
+let decimal2 = myNum.toFixed(2);
+
+
+let myBigInt = 123n;
+myBigInt += 1n;
