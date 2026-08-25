476904f3867ae3a02d40af2d0e11004307cccc1e
vITALIY-jj <vitalijsogenko@gmail.com>
2026-08-25 15:48:17 +0300

    homework 2

---
+++ js-homework/home work 2.js
@@ -0,0 +1,26 @@
+// random 
+const min = 1;
+const max = 90;
+function random( min , max ) {
+    return Math.floor(Math.random() * (max - min +1)) + min
+}
+
+// hello mesage
+const msg = hi;
+const username = Vitalii; 
+ function greet( msg , username ) {
+    return msg + ' , ' + username; 
+ }
+
+// big number 
+const bgn = 2345234523452435245n;
+const bgn2 = 5234234523452345225n; 
+
+function sumBigIntegers( bgn , bgn2) {
+    return BigInt(bgn) + BigInt(bgn2);
+}
+
+//\
+
+
+
