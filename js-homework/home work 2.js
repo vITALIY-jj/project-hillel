5b4bd66e516bec98994b6d227acbb9e8bb7eab58
vITALIY-jj <vitalijsogenko@gmail.com>
2026-09-03 21:41:32 +0300

    homework 1st september

--- js-homework/home work 2.js
+++ js-homework/home work 2.js
@@ -1,29 +1,51 @@
-// 1
+console.log('#3. JavaScript homework example file')
 
-function getRectangleArea(width, height) {
-    return width * height;
-}
+/*
+ * #1
+ *
+ * Створіть об'єкт userObj, що описує людину.
+ *
+ * Наступні поля обов'язкові:
+ * firstName - будь-яке ім'я, рядок
+ * lastName - будь-яке прізвище, рядок
+ * age - будь-який вік, число
+ */
 
-console.log (getRectangleAre(3, 14))
-console.log (getRectangleAre(5, 24))
-console.log (getRectangleAre(1, 56))
+const userObj = {
+    firstName: "vitalii",
+    lastName: "sosnenko",
+    age: 18
+};
 
-//2 
+console.log(userObj)
 
-function applyDiscount(price, discount =0) {
-    return price - price * discount /100;
-}
+/*
+ * #2
+ *
+ * Для об'єкта з п.1 створіть метод fullName(), що повертає коректне повне ім'я, яке є конкатенацією firstName та lastName через пробіл.
 
-console.log(applyDiscount(1000, 50))
+ * Наприклад:
+ * userObj.firstName ← 'John'
+ * userObj.lastName  ← 'Smith'
+ * userObj.fullName() → 'John Smith'.
+ */
 
-//3
+userObj.fullName = function () {
+    return this.firstName + " " + this.lastName
+};
 
-function greet(name) {
-    function capitalize(name) {
-        return str[0].toUpperCase() + str.slice(1).toLowerCase()
-    }
+// console.log(userObj.fullName()) // John Smith
 
-    return "Hello, " + capitalize(name) + "!";
-}
+/*
+ * #3
+ *
+ * Функція defUpperStr('My text') повертає текст, перетворений у верхній регістр, тобто: defUpperStr('My text') → 'MY TEXT'.
+ *
+ * Якщо функція викликається без параметра defUpperStr(), вона не повинна повертати undefined, у цьому випадку потрібно повернути рядок тексту за замовчуванням у верхньому регістрі, тобто defUpperStr() → 'DEFAULT TEXT'.
+ *
+ * При виконанні завдання не використовуйте оператор if, потрібен розв'язок із логічним оператором ||.
+ */
 
-console.log(greet("витАЛИй"))
+function defUpperStr(text) {
+    return (text || "defaul text").toUpperCase
+};
