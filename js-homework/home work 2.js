28b5417f90b966ce87ea569938250a42ebe0c484
vITALIY-jj <vitalijsogenko@gmail.com>
2026-09-18 16:08:50 +0300

    home work 7

--- js-homework/home work 2.js
+++ js-homework/home work 2.js
@@ -1,56 +1,130 @@
 //1 
-const phoneBook = new Map();
-phoneBook.set("Оля", "050-111-22-33");
-phoneBook.set("Іван", "067-444-55-66");
-phoneBook.set("Марія", "093-777-88-99");
+const queue = ['Аня', 'Богдан']; 
 
-const findPhone = (name) => phoneBook.get(name) || "unknown contact";
+queue.push('gleb');
+queue.unshift('vera');
+//console.log(queue, queue.length);
+//2 
+const numbers = [10, 20, 30, 40];
 
-const removeContact = (name) => {
-    if (phoneBook.has(name)) {
-        phoneBook.delete(name);
-        console.log("delete complete")
-    }
-        else {
-            console.log("unknown contact")   
-    }
+const first = numbers.pop();
+const last = numbers.shift();
 
-};
-
-phoneBook.forEach((phone, name) => {
-console.log(`${name}: ${phone}`);
-
-});
-//2
-
-const allowedCodes = new Set();
-allowedCodes.add(101);
-allowedCodes.add(205);
-allowedCodes.add(333);
+const result = () => first + last;  
 
 
-const checkCode = (code) => {
-    if (allowedCodes.has(code)) {
-        console.log("access granted")
+//console.log(numbers, first, last, result());
+//3 
+
+const tasks = [
+    { id: 4, title: 'make a project', done: false },
+    { id: 6, title: 'complete homework', done: true },
+    { id: 7, title: 'buy a car', done: false },
+    { id: 2, title: 'clean my room', done: true }
+]
+
+tasks.shift();
+tasks.unshift({ id: 3, title: 'Купити хліб', done: false });
+
+//console.log(tasks);
+//4
+
+const users = [
+    {id: 1, name: 'john'},
+    {id: 2, name: 'vitalii'},
+]
+
+const adduser = (name) => {
+    const userExists = users.some(user => user.name === name); 
+
+    if (userExists) {
+        //console.log ('user is exists');
     } else {
-        console.log("access denied")
+        users.push({id:Math.random(), name: name});
+        //console.log('ser is added')
     }
-};
-//3 доп задание от gpt 
+}; 
 
-const visitors = new Set();
+//5 
 
-visitors.add("Оля");
-visitors.add("Іван");
-visitors.add("Марія");
-visitors.add("Оля");
+const task5 = [10, 50 ,20, 6, 5, 7, 24, ];
+    
+const result2 = task5.filter(number => number > 10);
+//6 
 
-const addVisitor = (name) => {
-    if (!visitors.has(name)) {
-        visitors.add(name);
-        console.log("visitor added")
-    }
-    else {
-        console.log("visitor access")
+const pets = ['cat', 'dog', 'rabbit', 'fish'];
+
+const petsResult = pets.filter(pet => pet.length >= 4);
+
+//console.log(petsResult);
+//7 
+
+const minusNumber = [0, 5, 6, -7, -45];
+
+const result3 = () => {
+   const found = minusNumber.find(number => number < 0);
+   if (found) {
+    console.log(found);
+   }
+
+     else {
+        console.log('number not found')
     }
 }
+//result3()
+//8/9
+const users2 = [
+  { id: 1, name: 'Аня', age: 25 },
+  { id: 2, name: 'Богдан', age: 17 },
+  { id: 3, name: 'Віра', age: 32 },
+];
+
+const findUserId = () => {
+     const findUser = users2.find(user => user.id === 6); 
+     if (findUser) {
+        console.log(findUser.name);
+     } else {
+        console.log('user not found');
+     }
+           
+};
+
+//findUserId();
+
+//10 
+const products1 = [
+  { name: 'Ноутбук', price: 30000, inStock: true },
+  { name: 'Миша', price: 800, inStock: false },
+  { name: 'Клавіатура', price: 2500, inStock: false },
+];
+
+//const findProduct = () => {
+    //const inStockProduct = products.find(product => product.inStock === false);
+    //if (inStockProduct) {
+        //console.log(inStockProduct.name);
+   // }
+//};
+
+//findProduct();
+//11
+const kitty = ["кот", "конь", "собака"]; 
+
+const result4 = kitty.filter(word => word.startsWith('к'));
+
+//console.log(result4);
+//13
+const products = [
+  { name: 'Ноутбук', price: 30000, inStock: true },
+  { name: 'Миша', price: 800, inStock: false },
+  { name: 'Клавіатура', price: 2500, inStock: true },
+  { name: 'Килимок', price: 300, inStock: true },
+];
+
+const findProduct2 = () => {
+    const productPrice = products.filter(product => product.price < 5000)
+    if (productPrice) {
+        console.log(productPrice)
+    }
+}
+
+findProduct2();
